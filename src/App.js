149d7865d7cf6f48c3e189e661c1a6cdd00f5cd5
camposLuis <luis.campos.henrique@hotmail.com>
2019-08-15 22:18:40 -0300

    Importando imagens

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
 import "./App.css";
 
+import profile from "./assets/luis.png";
+
 function App() {
-  return <h1>Hello Rocketseat</h1>;
+  return <img src={profile} />;
 }
 
 export default App;
